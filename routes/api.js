06cf173b8089ebc9841c47fb7f4f91202524184c
rlawndks4 <rlawndks0423@naver.com>
2022-09-24 02:40:47 +0900

    asdf

--- routes/api.js
+++ routes/api.js
@@ -984,7 +984,7 @@ const getVideoContent = (req, res) => {
 const getComments = (req, res) => {
     try {
         const { pk, category} = req.query;
-        db.query("SELECT comment_table.*, user_table.nickname, user_table.profile_img FROM comment_table LEFT JOIN user_table ON comment_table.user_pk = user_table.pk WHERE comment_table.item_pk=? AND comment_table.category_pk=? ORDER BY pk DESC",[pk, category],(err, result)=>{
+        db.query("SELECT comment_table.*, user_table.nickname, user_table.profile_img FROM comment_table LEFT JOIN user_table ON comment_table.user_pk = user_table.pk WHERE comment_table.item_pk=? AND comment_table.category_pk=? ORDER BY pk ASC",[pk, category],(err, result)=>{
             if (err) {
                 console.log(err)
                 response(req, res, -200, "fail", [])
