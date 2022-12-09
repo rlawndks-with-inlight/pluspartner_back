005e756e59afe3f0f73367911091049d82920fb4
rlawndks4 <rlawndks0423@naver.com>
2022-12-09 09:43:21 +0900

    asfd

--- web.js
+++ web.js
@@ -161,16 +161,16 @@ const resizeFile = async (path, filename) => {
 
 app.get('/api/item', async (req, res) => {
         try {
-                console.log(req.query)
-                if (tooMuchRequest(app.connectionsN)) {
-                        return response(req, res, -120, "접속자 수가 너무많아 지연되고있습니다.(잠시후 다시 시도 부탁드립니다.)", [])
-                }
+                // console.log(req.query)
+                // if (tooMuchRequest(app.connectionsN)) {
+                //         return response(req, res, -120, "접속자 수가 너무많아 지연되고있습니다.(잠시후 다시 시도 부탁드립니다.)", [])
+                // }
                 let table = req.query.table ?? "user";
                 console.log(table)
                 let pk = req.query.pk ?? 0;
                 let whereStr = " WHERE pk=? ";
                 const decode = checkLevel(req.cookies.token, 0)
-                if ((!decode ||decode?.user_level==-10) && table !== 'notice') {
+                if ((!decode || decode?.user_level==-10) && table !== 'notice') {
                         return response(req, res, -150, "권한이 없습니다.", [])
                 }
                 if (table == "setting") {
@@ -212,9 +212,9 @@ app.get('/api/item', async (req, res) => {
 
 app.get('/api/getvideocontent', (req, res) => {
         try {
-                if (tooMuchRequest(app.connectionsN)) {
-                        return response(req, res, -120, "접속자 수가 너무많아 지연되고있습니다.(잠시후 다시 시도 부탁드립니다.)", [])
-                }
+                // if (tooMuchRequest(app.connectionsN)) {
+                //         return response(req, res, -120, "접속자 수가 너무많아 지연되고있습니다.(잠시후 다시 시도 부탁드립니다.)", [])
+                // }
                 const decode = checkLevel(req.cookies.token, 0)
                 if (!decode) {
                         return response(req, res, -150, "권한이 없습니다.", [])
